c687c526c9c5683dd974508bea1b796a6561386f
Keun-young Park <keunyoung@google.com>
2018-12-07 14:23:50 -0800

    Add force off mode to ltc1695
    - Setting 1 to sys fs pwm_off1 forces it to be off until
      device become much hotter and value of 100 is written.
      (# echo '1' > /sys/class/hwmon/hwmon1/pwm_off1)
    - 100 is treated as a special value meaning force on. For this value,
      force off will be ignored and fan will run in max speed.
      Also force off state will be cleared as force off state
      is not expected to last for too long.
    - Upper layer can use this to slience fan for short time.
    - Update phanbell h dts to force max the fan for temperature
      above 70 degrees. If it is too hot, do not allow force off.
    
    Bug: 120574255
    Test: test by writing values to sys fs and also by loading system.
    Change-Id: Ieb6776eaddd311b5f068a46457cdd0000b3a26c2

--- drivers/hwmon/ltc1695.c
+++ drivers/hwmon/ltc1695.c
@@ -24,6 +24,7 @@
 #include <linux/thermal.h>
 
 
+
 struct ltc1695_data {
 	struct i2c_client *client;
 
@@ -31,18 +32,40 @@ struct ltc1695_data {
 	struct thermal_cooling_device *cdev;
 
 	u8 pwm_value;
+	// flag to keep pwm in low value
+	u8 force_pwm_off;
 };
 
 #define PWM_VALUE_MAX 0x3f
+#define PWM_VALUE_FORCE_MAX 100
 #define PWM_BOOST_START_TIMER_FLAG 0x40
 
 
-static int ltc1695_set_pwm_reg(struct i2c_client *client, u8 value)
+static int ltc1695_set_pwm_reg(struct ltc1695_data *data)
 {
-	u8 reg_value = value | PWM_BOOST_START_TIMER_FLAG;
-	return i2c_smbus_write_byte(client, reg_value);
+	u8 reg_value;
+	if (data->pwm_value == PWM_VALUE_FORCE_MAX) {
+		pr_info("ltc1695 set_pwm force max\n");
+		reg_value = PWM_VALUE_MAX;
+	} else if (data->force_pwm_off) {
+		reg_value = 0;
+		pr_info("ltc1695 set_pwm force off\n");
+	} else {
+		reg_value = data->pwm_value;
+		pr_info("ltc1695 set_pwm set requested value:%d\n", data->pwm_value);
+	}
+	reg_value |= PWM_BOOST_START_TIMER_FLAG;
+	return i2c_smbus_write_byte(data->client, reg_value);
 }
 
+static u8 limit_pwm_value_range(u8 value) {
+	if (value == PWM_VALUE_FORCE_MAX) {
+		return value;
+	} else if (value > PWM_VALUE_MAX) {
+		return PWM_VALUE_MAX;
+	}
+	return value;
+}
 
 static ssize_t get_pwm(struct device *dev, struct device_attribute *da,
 		       char *buf)
@@ -52,26 +75,53 @@ static ssize_t get_pwm(struct device *dev, struct device_attribute *da,
 	return sprintf(buf, "%d\n", data->pwm_value);
 }
 
+
 static ssize_t set_pwm(struct device *dev, struct device_attribute *da,
 		       const char *buf, size_t count)
 {
 	struct ltc1695_data *data = dev_get_drvdata(dev);
-	struct i2c_client *client = data->client;
 	unsigned long val;
 
 	if (kstrtoul(buf, 10, &val))
 		return -EINVAL;
-	data->pwm_value = clamp_val(val, 0, PWM_VALUE_MAX);
+	data->pwm_value = limit_pwm_value_range(val);
 	pr_info("ltc1695 set_pwm %d\n", data->pwm_value);
-	ltc1695_set_pwm_reg(client, data->pwm_value);
+	ltc1695_set_pwm_reg(data);
 
 	return count;
 }
 
 static DEVICE_ATTR(pwm1, S_IWUSR | S_IRUGO, get_pwm, set_pwm);
 
+static ssize_t get_pwm_off(struct device *dev, struct device_attribute *da,
+		       char *buf)
+{
+	struct ltc1695_data *data = dev_get_drvdata(dev);
+
+	return sprintf(buf, "%d\n", data->force_pwm_off);
+}
+
+static ssize_t set_pwm_off(struct device *dev, struct device_attribute *da,
+		       const char *buf, size_t count)
+{
+	struct ltc1695_data *data = dev_get_drvdata(dev);
+	unsigned long val;
+
+	if (kstrtoul(buf, 10, &val))
+		return -EINVAL;
+	data->force_pwm_off = clamp_val(val, 0, 1);
+	pr_info("ltc1695 force pwm off:%d\n", data->force_pwm_off);
+
+	ltc1695_set_pwm_reg(data);
+
+	return count;
+}
+
+static DEVICE_ATTR(pwm_off1, S_IWUSR | S_IRUGO, get_pwm_off, set_pwm_off);
+
 static struct attribute *ltc1695_attrs[] = {
 	&dev_attr_pwm1.attr,
+	&dev_attr_pwm_off1.attr,
 	NULL
 };
 
@@ -116,9 +166,13 @@ static int ltc1695_set_cur_state(struct thermal_cooling_device *cdev, unsigned l
 		return 0;
 	}
 
-	data->pwm_value = clamp_val(state, 0, PWM_VALUE_MAX);
-	pr_info("ltc1695 set_pwm %d\n", data->pwm_value);
-	ltc1695_set_pwm_reg(data->client, data->pwm_value);
+	data->pwm_value = limit_pwm_value_range(state);
+	pr_info("ltc1695 set_cur_state, pwm:%d\n", data->pwm_value);
+	ltc1695_set_pwm_reg(data);
+	if (data->pwm_value == PWM_VALUE_FORCE_MAX && data->force_pwm_off) {
+		pr_info("Cleariing force_pwm_off as device is too hot\n");
+		data->force_pwm_off = 0;
+	}
 
 	return 0;
 }
@@ -150,8 +204,8 @@ static int ltc1695_probe(struct i2c_client *client,
 	data->client = client;
 
 	data->pwm_value = PWM_VALUE_MAX;
-	ltc1695_set_pwm_reg(client, data->pwm_value);
-	pr_info("Found ltc1695, id 0x%x, current pwm:%d\n", id->driver_data,
+	ltc1695_set_pwm_reg(data);
+	pr_info("Found ltc1695, id 0x%lx, current pwm:%d\n", id->driver_data,
 		data->pwm_value);
 
 	hwmon_dev = devm_hwmon_device_register_with_groups(dev, "ltc1695",
